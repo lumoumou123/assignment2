203c1e3e5fdb242a6bdf848e820c6593a0c70335
lumoumou123 <20109114@mail.wit.ie>
2025-03-31 01:12:47 +0100

    Started Fan out for S3 image upload.

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -30,10 +30,13 @@ export class EDAAppStack extends cdk.Stack {
 
       // Integration infrastructure
 
-  const queue = new sqs.Queue(this, "img-created-queue", {
-    receiveMessageWaitTime: cdk.Duration.seconds(5),
-  });
-
+      const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
+        receiveMessageWaitTime: cdk.Duration.seconds(10),
+      });
+      const newImageTopic = new sns.Topic(this, "NewImageTopic", {
+        displayName: "New Image topic",
+      }); 
+  
   // Lambda functions
 
   const processImageFn = new lambdanode.NodejsFunction(
@@ -46,18 +49,24 @@ export class EDAAppStack extends cdk.Stack {
       memorySize: 128,
     }
   );
+   
+  newImageTopic.addSubscription(
+    new subs.SqsSubscription(imageProcessQueue)
+  );
 
   // S3 --> SQS
   imagesBucket.addEventNotification(
     s3.EventType.OBJECT_CREATED,
-    new s3n.SqsDestination(queue)
-  );
+    new s3n.SnsDestination(newImageTopic)  // Changed
+);
+
 
  // SQS --> Lambda
-  const newImageEventSource = new events.SqsEventSource(queue, {
-    batchSize: 5,
-    maxBatchingWindow: cdk.Duration.seconds(5),
-  });
+ const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
+  batchSize: 5,
+  maxBatchingWindow: cdk.Duration.seconds(5),
+});
+
 
   processImageFn.addEventSource(newImageEventSource);
 
